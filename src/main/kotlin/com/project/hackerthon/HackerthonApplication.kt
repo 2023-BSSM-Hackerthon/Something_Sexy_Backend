67cd2b1d492fa7889bd33f8689ce7160d2c31d15
마현우 <mhw35689402@gmail.com>
2023-12-27 19:42:06 +0900

    chore: component scan

--- src/main/kotlin/com/project/hackerthon/HackerthonApplication.kt
+++ src/main/kotlin/com/project/hackerthon/HackerthonApplication.kt
@@ -1,11 +1,13 @@
 package com.project.hackerthon
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
 @EnableJpaAuditing
+@ConfigurationPropertiesScan
 class HackerthonApplication
 
 fun main(args: Array<String>) {
