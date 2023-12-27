05ad3f406d3f53db491f2a3eb152f5e3d3ca9171
J1W0N-1209 <nabook754@naver.com>
2023-12-27 14:18:10 +0900

    init

---
+++ src/main/kotlin/com/project/hackerthon/HackerthonApplication.kt
@@ -0,0 +1,11 @@
+package com.project.hackerthon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HackerthonApplication
+
+fun main(args: Array<String>) {
+	runApplication<HackerthonApplication>(*args)
+}
