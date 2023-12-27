664eb422c973fa4acb7c1468d55a1b163923a3bc
J1W0N-1209 <nabook754@naver.com>
2023-12-27 16:41:43 +0900

    entity 구현

---
+++ src/main/kotlin/com/project/hackerthon/domain/form/Form.kt
@@ -0,0 +1,39 @@
+package com.project.hackerthon.domain.form
+
+import com.project.hackerthon.domain.user.User
+import com.project.hackerthon.global.entity.BaseDateTime
+import jakarta.persistence.*
+
+@Entity
+class Form (
+    title: String,
+    content: String,
+    theme: ThemeType,
+    author: User
+): BaseDateTime() {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0L
+
+    @Column(nullable = false)
+    var title: String = title
+        protected set
+
+    @Column(columnDefinition = "TEXT")
+    var content: String = content
+        protected set
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    var theme: ThemeType = theme
+        protected set
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+        name = "user_id",
+        nullable = false,
+        foreignKey = ForeignKey(name = "FK_FORM_USER_ID")
+    )
+    var author: User = author
+        protected set
+}
