457623360a7c39baad9952ffda43b36b1632d972
마현우 <mhw35689402@gmail.com>
2023-12-27 19:41:42 +0900

    add: security 세팅

---
+++ src/main/kotlin/com/project/hackerthon/global/security/SecurityConfig.kt
@@ -0,0 +1,66 @@
+package com.project.hackerthon.global.security
+
+import com.project.hackerthon.global.error.ExceptionFilter
+import com.project.hackerthon.global.jwt.filter.JwtFilter
+import com.project.hackerthon.global.jwt.util.JwtUtil
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod.*
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer
+import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig(
+    private val jwtUtil: JwtUtil,
+) {
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        return http
+            .cors(Customizer.withDefaults())
+            .httpBasic { obj: HttpBasicConfigurer<HttpSecurity> -> obj.disable() }
+            .formLogin { obj: FormLoginConfigurer<HttpSecurity> -> obj.disable() }
+            .csrf { obj: CsrfConfigurer<HttpSecurity> -> obj.disable() }
+            .sessionManagement { session: SessionManagementConfigurer<HttpSecurity> ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+
+            .authorizeHttpRequests { authorize -> authorize
+                .requestMatchers(GET, "/api/form").hasAuthority("TEACHER")
+                .requestMatchers(GET, "/api/form/**").hasAuthority("TEACHER")
+                .requestMatchers(PATCH, "/api/form").hasAuthority("TEACHER")
+                .requestMatchers(POST, "/api/result/**").hasAuthority("TEACHER")
+                .requestMatchers("/api/form", "/api/result/**").authenticated()
+                .anyRequest().permitAll() }
+
+            .addFilterBefore(JwtFilter(jwtUtil), UsernamePasswordAuthenticationFilter::class.java)
+            .addFilterBefore(ExceptionFilter(), JwtFilter::class.java)
+
+            .build()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.addAllowedOriginPattern("*")
+        configuration.addAllowedMethod("*")
+        configuration.addAllowedHeader("*")
+        configuration.allowCredentials = true
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+
+        return source
+    }
+}
