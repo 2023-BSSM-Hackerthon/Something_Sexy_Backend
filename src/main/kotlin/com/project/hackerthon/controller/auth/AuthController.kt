a5f0d67fce9033d6893c450d8cfe624b52ec574b
마현우 <mhw35689402@gmail.com>
2023-12-28 11:15:36 +0900

    add: user info api 추가

--- src/main/kotlin/com/project/hackerthon/controller/auth/AuthController.kt
+++ src/main/kotlin/com/project/hackerthon/controller/auth/AuthController.kt
@@ -1,7 +1,11 @@
 package com.project.hackerthon.controller.auth
 
 import com.project.hackerthon.controller.auth.dto.response.LoginResponseDto
+import com.project.hackerthon.domain.user.User
 import com.project.hackerthon.service.auth.AuthService
+import com.project.hackerthon.service.user.UserGetService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -11,7 +15,11 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/auth")
 class AuthController(
     private val authService: AuthService,
+    private val userGetService: UserGetService,
 ) {
+
+    @GetMapping
+    fun getInfo(): ResponseEntity<User> = ResponseEntity.ok(userGetService.findOne())
 
     @PostMapping("/student")
     fun loginStudent(@RequestParam(name = "code") code: String): LoginResponseDto = authService.student(code)
